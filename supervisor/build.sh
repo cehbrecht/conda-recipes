2bb6ec40bd0a5a5473aade1cde5e18fd44596f9f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-23 17:46:01 +0200

    working on supervisor

--- supervisor/build.sh
+++ supervisor/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # setup environment
+mkdir -vp ${PREFIX}/bin || exit 1;
+mkdir -vp ${PREFIX}/etc/supervisor || exit 1;
+mkdir -vp ${PREFIX}/etc/supervisor/conf.d || exit 1;
 mkdir -vp ${PREFIX}/var/run || exit 1;
 mkdir -vp ${PREFIX}/var/log/supervisor || exit 1;
 touch ${PREFIX}/var/log/supervisor/supervisord.log || exit 1;
 
-
+# install
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
