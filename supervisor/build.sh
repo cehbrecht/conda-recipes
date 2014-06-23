4ac73833665036e92f80130e4377b8dd308f36a8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-23 15:45:17 +0200

    build env for supervisor

--- supervisor/build.sh
+++ supervisor/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# setup environment
+mkdir -vp ${PREFIX}/var/run || exit 1;
+mkdir -vp ${PREFIX}/var/log/supervisor || exit 1;
+touch ${PREFIX}/var/log/supervisor/supervisord.log || exit 1;
+
+
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
