1d8ec203d603c16e4816013f73925f8aeaac636c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-30 13:54:37 +0200

    apache-tomcat build works now

--- apache-tomcat/build.sh
+++ apache-tomcat/build.sh
@@ -5,10 +5,14 @@
 # - appropriately setup JAVA_HOME variable in system
 
 mkdir -vp ${PREFIX}/bin;
-mkdir -vp ${PREFIX}/opt;
+mkdir -vp ${PREFIX}/opt/apache-tomcat;
 
-export JAVA_HOME="/usr/lib/jvm/java"
-export JRE_HOME="/usr/lib/jvm/jre"
+export JAVA_HOME="/usr/lib/jvm/default-java"
+#export JRE_HOME="/usr/lib/jvm/jre"
 
-mv * ${PREFIX}/opt/
+mv * ${PREFIX}/opt/apache-tomcat
+
+pushd ${PREFIX}/bin || return 1;
+ln -vs ../opt/apache-tomcat/bin/catalina.sh . || return 1;
+popd || return 1;
 
