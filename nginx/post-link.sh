90dd745ecb8f8c0a1364180536ef4861c3f545ab
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-02 15:37:31 +0200

    link nginx default html path to var/www

--- nginx/post-link.sh
+++ nginx/post-link.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# link default nginx web pages to var/www
+if [ ! -e "${PREFIX}/var/www" ]; then
+    ln -s "${PREFIX}/html" "${PREFIX}/var/www"
+fi
+
 cat <<EOF >> ${PREFIX}/.messages.txt
 This is an install message from the nginx package.
 EOF
