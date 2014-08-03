5b317449df92cc155a4c46c3221ab92fbafd7bb1
Carsten Ehbrecht <carsten@linacs.org>
2014-08-03 18:29:57 +0200

    working on macosx build of cdo

--- cdo/build.sh
+++ cdo/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ./configure --prefix=$PREFIX \
-    --enable-shared \
+    --disable-debug \
+    --disable-dependency-tracking \
     --with-jasper=$PREFIX \
     --with-grib_api=$PREFIX \
+    --with-hdf5=$PREFIX \
     --with-netcdf=$PREFIX \
     --with-proj=$PREFIX
 make
