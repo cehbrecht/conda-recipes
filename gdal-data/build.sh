15b5d6da14ba6b0f40cd9b13fccbc9b728b0b51a
Carsten Ehbrecht <carsten@linacs.org>
2015-01-11 20:55:44 +0100

    moved gdal-data to birdhouse

--- gdal-data/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir $PREFIX/share
-mkdir $PREFIX/share/gdal
-cp data/* $PREFIX/share/gdal
-cp LICENSE.TXT $PREFIX/share/gdal
-mkdir $PREFIX/bin    # HACK to get post-link script to copy.
--- gdal-data/post-link.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "Must set environmental variable:"
-echo "export GDAL_DATA=$PREFIX/share/gdal"
